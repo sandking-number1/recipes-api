34f021350204528b69ca82171789b72426d4da1e
sandking-number1 <Alfred.J9412@gmail.com>
2018-03-01 17:05:30 -0700

    Configured redux for application

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import configureStore from './store/configureStore';
+
 import App from './components/App';
 
+const store = configureStore();
+
 ReactDOM.render(
-    <App />, document.querySelector('#root'));
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
