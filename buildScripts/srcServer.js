a8f8368db7a494279cb568c73ff133a857712c58
sandking-number1 <Alfred.J9412@gmail.com>
2018-03-11 01:59:48 -0700

    using path now

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,5 +1,5 @@
 import express from 'express';
-// import path from 'path';
+import path from 'path';
 import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.babel';
