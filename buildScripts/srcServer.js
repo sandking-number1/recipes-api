048a769f61bd342901a50d155353ba84a5f50a82
sandking-number1 <Alfred.J9412@gmail.com>
2018-03-13 17:58:12 -0600

    access-control

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,9 +19,10 @@ const port = process.env.PORT || 3030;
 const app = express();
 
 app.use((req,res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://arcane-castle-79035.herokuapp.com");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Contorol-Allow-Methods", "GET, POST, OPTIONS");
+  res.header('Access-Control-Allow-Origin', 'https://arcane-castle-79035.herokuapp.com');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', req.header('access-control-request-headers'));
   next();
 });
 
