37cfb1fc259796d7349a092202129819f602613d
sandking-number1 <Alfred.J9412@gmail.com>
2018-03-13 18:01:16 -0600

    cookieSession

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -29,8 +29,7 @@ app.use((req,res, next) => {
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey],
-    httpOnly = false
+    keys: [keys.cookieKey]
   })
 );
 
